774691a65308722a8b6a796982bca06b40242939
kaikis <kueobei@gmail.com>
2019-07-16 14:38:17 +0900

    commit

--- sw.js
+++ sw.js
@@ -5,13 +5,13 @@ console.log('[ServiceWorker] Install');
 
 self.addEventListener('activate', function(e) {
 console.log('[ServiceWorker] Activate');
-});
-
-// 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
-
 var temp = 0;
 setInterval(function(){
     console.log(temp);
     temp+=1;
 }, 1000);
+});
+
+// 現状では、この処理を書かないとService Workerが有効と判定されないようです
+self.addEventListener('fetch', function(event) {});
+
